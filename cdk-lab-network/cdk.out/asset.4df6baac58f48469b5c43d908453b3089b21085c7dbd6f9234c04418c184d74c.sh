156d188d3ab1761e985782ddcb8097318cc842dd
JingJing <awyongjj@hotmail.com>
2024-10-20 21:29:59 -0500

    del old files

--- cdk-lab-network/cdk.out/asset.4df6baac58f48469b5c43d908453b3089b21085c7dbd6f9234c04418c184d74c.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Use this to install software packages
-# This script is for EC2 userdata. All commands are executed as administrators. 
-yum update -y
-amazon-linux-extras install mariadb10.5
-amazon-linux-extras install php8.2
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
